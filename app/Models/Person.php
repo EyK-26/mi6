927b10941391b6cbf2f5fb0a26b4504405e90201
EyK-26 <137394243+EyK-26@users.noreply.github.com>
2023-11-06 13:03:15 +0100

    correction

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,20 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Person extends Model
 {
     use HasFactory;
 
-    public function aliases()
+    public function aliases(): HasMany
     {
         return $this->hasMany(Alias::class);
     }
-    public function image()
+    public function image(): BelongsTo
     {
         return $this->belongsTo(Image::class);
     }
-    public function status()
+    public function status(): BelongsTo
     {
         return $this->belongsTo(Status::class);
     }
