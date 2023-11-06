debffb770e3fb55982773115e83765697f423dc3
EyK-26 <137394243+EyK-26@users.noreply.github.com>
2023-11-06 20:32:02 +0100

    correct config file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 import laravel from "laravel-vite-plugin";
 
 export default ({ mode }) => {
@@ -15,7 +15,7 @@ export default ({ mode }) => {
 
                 refresh: true,
             }),
-            react(),
+
             {
                 // fixes URLs that start with / in CSS files
 
