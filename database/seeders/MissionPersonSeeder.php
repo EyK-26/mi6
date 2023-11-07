31418c8c0416146f51366d468ee15cdc359de075
EyK-26 <137394243+EyK-26@users.noreply.github.com>
2023-11-07 13:17:10 +0100

    add seeder for intermediate table

---
+++ database/seeders/MissionPersonSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MissionPersonSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+        // DB::table("mission_person")->truncate();
+
+        while (DB::table("mission_person")->count() < 50) {
+            DB::table("mission_person")->insert(['mission_id' => rand(1, 25), 'person_id' => rand(1, 319)]);
+        }
+    }
+}
