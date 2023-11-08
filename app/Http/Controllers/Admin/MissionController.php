4985f5cffb4779a56500ce61e7b640793e6d3bb7
EyK-26 <137394243+EyK-26@users.noreply.github.com>
2023-11-08 10:13:01 +0100

    exc

--- app/Http/Controllers/Admin/MissionController.php
+++ app/Http/Controllers/Admin/MissionController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Mission;
 use Illuminate\Http\Request;
 
 class MissionController extends Controller
 {
-    //
+    public function edit($mission_id)
+    {
+        $mission = Mission::findOrFail($mission_id);
+        return view("admin.missions.edit", compact("mission"));
+    }
 }
